48348b5a8d7910294879264a3a6e26b23338fbf9
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-06-09 19:01:07 -0300

    checkpoint

--- MButton_Base/ContentView.swift
+++ MButton_Base/ContentView.swift
@@ -10,15 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            // custom button
-            MButton(text: "Let's go", style: .tertiary)
             
-            // button style
-            Button("Custom text larger than ever thought") {
-                print("Button pressed!")
+            // primary button
+            Button("Primary Button") {
+                print("Primary Button pressed!")
             }
             .buttonStyle(MButtonStyle(style: .primary))
             
+            // secondary button
+            Button("Secondary Button") {
+                print("Secondary Button pressed!")
+            }
+            .buttonStyle(MButtonStyle(style: .secondary))
+            
+            // tertiary button
+            MButton(text: "Tertiary Button", style: .tertiary)
+            
+        
         }
 
     }
