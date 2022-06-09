b92d5558ad36b2053938f4e5a100b6fac6c3697f
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-06-09 15:49:17 -0300

    Initial Commit

---
+++ MButton_Base/MButton_BaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MButton_BaseApp.swift
+//  MButton_Base
+//
+//  Created by Marcelo Sampaio on 09/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MButton_BaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
